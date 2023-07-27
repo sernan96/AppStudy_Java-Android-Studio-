9213502e260e2a982a1d4734b7536d26fd05d1f7
Hwang gend <80672346+sernan96@users.noreply.github.com>
2023-07-27 23:42:42 +0900

    intent 활용

---
+++ Call_Intent.java
@@ -0,0 +1,34 @@
+package com.example.mycallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText)findViewById (R.id.editText);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String receiver = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+receiver));//명시적 intent
+                startActivity(intent);
+            }
+        });
+    }
+}
