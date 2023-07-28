27a844d27ed58a7bf6465a2643d864d950a456e4
Hwang gend <80672346+sernan96@users.noreply.github.com>
2023-07-28 14:47:39 +0900

    parcelable 활용
    인터페이스 SimpleData

---
+++ parcelable_SimpleData.java
@@ -0,0 +1,47 @@
+package com.example.myparcelable;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class SimpleData implements Parcelable{
+
+
+    int number;
+    String message;
+
+    public SimpleData(int number, String message) {
+        this.number = number;
+        this.message = message;
+    }
+
+    public SimpleData(Parcel src){
+        number = src.readInt();
+        message = src.readString();
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public SimpleData createFromParcel(Parcel src) {
+            return new SimpleData(src);
+        }
+
+        @Override
+        public SimpleData[] newArray(int size) {
+            return new SimpleData[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(number);
+        dest.writeString(message);
+    }
+}
